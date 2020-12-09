ba5ede8b33a47379e36ad14a83057acc14dfb831
karthik <skarthikkumarr@gmail.com>
2020-12-09 18:29:37 +0530

    Adding checkbox

---
+++ src/component/Checkbox.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../scss/checkbox.scss";
+
+const Checkbox = () => {
+  return (
+    <div className='bg'>
+      <div class='checkbox-container'>
+        <input type='checkbox' id='cb3' />
+        <label for='cb3'></label>
+      </div>
+    </div>
+  );
+};
+
+export default Checkbox;
--- src/containers/TaskContainer.js
+++ src/containers/TaskContainer.js
@@ -1,5 +1,6 @@
 import Content from "../component/Content";
 import Footer from "../component/Footer";
+import Checkbox from "../component/Checkbox";
 
 function TaskContainer() {
   return (
@@ -7,6 +8,7 @@ function TaskContainer() {
       <div className='card  mb-3'>
         <div className='card-body'>
           <h1 className='card-title text-center'>Things To Do</h1>
+          <Checkbox />
           <Content />
         </div>
         <Footer />
